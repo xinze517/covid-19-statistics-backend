66fb79049252a3b637e726052977270cbf9027c7
XINZE CHEN <46620280+xinze517@users.noreply.github.com>
2021-05-05 22:30:02 +0800

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,38 +1,24 @@
-# 创建身份表
-create table t_role
+create table confirmed_global
 (
-    id      bigint auto_increment primary key,
-    name    varchar(255) not null unique,
-    name_zh varchar(255) not null
+    id                bigint auto_increment comment '主键' primary key,
+    Province_or_State varchar(255) null comment '省份',
+    Country_or_Region varchar(255) not null comment '国家',
+    date              date         not null comment '日期',
+    count             int          not null comment '人数'
 );
-# 创建用户表
-create table t_user
+create table deaths_global
 (
-    id       bigint auto_increment primary key,
-    password varchar(255) not null,
-    username varchar(255) not null unique ,
-    enabled  bool         not null
+    id                bigint auto_increment comment '主键' primary key,
+    Province_or_State varchar(255) null comment '省份',
+    Country_or_Region varchar(255) not null comment '国家',
+    date              date         not null comment '日期',
+    count             int          not null comment '人数'
 );
-# 创建用户身份对应表
-create table t_users_roles
+create table recovered_global
 (
-    id      bigint auto_increment primary key,
-    user_id bigint not null,
-    role_id bigint not null,
-    constraint FK7l00c7jb4804xlpmk1k26texy
-        foreign key (user_id) references t_user (id),
-    constraint FKj47yp3hhtsoajht9793tbdrp4
-        foreign key (role_id) references t_role (id)
+    id                bigint auto_increment comment '主键' primary key,
+    Province_or_State varchar(255) not null comment '省份',
+    Country_or_Region varchar(255) not null comment '国家',
+    date              date         not null comment '日期',
+    count             int          not null comment '人数'
 );
-# 持久化令牌
-create table persistent_logins
-(
-    username  varchar(64) not null,
-    series    varchar(64) primary key,
-    token     varchar(64) not null,
-    last_used timestamp   not null
-);
-# 生成角色数据
-INSERT INTO t_role (name, name_zh)
-VALUES ('ROLE_admin', '管理员'),
-       ('ROLE_user', '普通用户');
